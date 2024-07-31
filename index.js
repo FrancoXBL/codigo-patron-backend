4fb42a49f6f9474855b35657736637cd6e7698f4
FranXBL <francoxavierbonin@hotmail.com>
2024-07-31 14:32:59 -0300

    cors

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 import express from "express";
 import connectDB from "./db.js";
-import cors from 'cors'
-import morgan from 'morgan'
-import history from 'connect-history-api-fallback'
-import dotenv from 'dotenv';
+import cors from "cors";
+import morgan from "morgan";
+import history from "connect-history-api-fallback";
+import dotenv from "dotenv";
 
-import newsRoutes from "./src/routes/news.routes.js"
-import matchRoutes from './src/routes/matches.routes.js'
-import userRoutes from './src/routes/admin.routes.js'
+import newsRoutes from "./src/routes/news.routes.js";
+import matchRoutes from "./src/routes/matches.routes.js";
+import userRoutes from "./src/routes/admin.routes.js";
 
 dotenv.config();
 
 const app = express();
 connectDB();
 
-app.use(cors());
-app.use(morgan('combined'));
+const whiteList = [
+  "https://codigo-patron-backend-production.up.railway.app",
+  "https://codigopatron.com.ar",
+];
+app.use(
+  cors({
+    origin: whiteList,
+  })
+);
+app.use(morgan("combined"));
 app.use(express.json());
 
-app.use("/api", newsRoutes)
-app.use("/api", matchRoutes)
-app.use("/api", userRoutes)
-app.use(history())
+app.use("/api", newsRoutes);
+app.use("/api", matchRoutes);
+app.use("/api", userRoutes);
+app.use(history());
 app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
