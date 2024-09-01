ba1ebcae722dffa40db3a19ca964458c66bc5938
FranXBL <francoxavierbonin@hotmail.com>
2024-09-01 14:27:22 -0300

    op

--- index.js
+++ index.js
@@ -19,19 +19,35 @@ const whiteList = [
   "https://codigo-patron.up.railway.app",
   "https://codigopatron.com.ar",
 ];
+
+// Configuración de CORS
 app.use(
   cors({
     origin: whiteList,
   })
 );
+
+// Configuración de morgan para logging
 app.use(morgan("combined"));
+
+// Middleware para parsear JSON
 app.use(express.json());
 
+// Rutas de la API
 app.use("/api", newsRoutes);
 app.use("/api", matchRoutes);
 app.use("/api", userRoutes);
+
+// Maneja el historial del enrutador para aplicaciones de una sola página
 app.use(history());
+
+// Configuración de archivos estáticos
 app.use(express.static("public"));
+
+// Ruta principal para servir el frontend
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
